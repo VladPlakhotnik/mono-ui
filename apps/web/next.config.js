e31608c4551516dc554743952d025b7ad1b1b1d0
Vlad <vladplakhotnikk@gmail.com>
2024-01-16 00:23:34 +0300

    feat: arch & added a few components

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  transpilePackages: ["@repo/ui"],
+  transpilePackages: ["@mono-ui/shared", "@mono-ui/button"],
 };
---
+++ packages/components/button/src/button.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { ReactNode } from "react";
+
+interface ButtonProps {
+  children: ReactNode;
+  className?: string;
+  appName: string;
+}
+
+const Button = ({ children, className, appName }: ButtonProps) => {
+  return (
+    <button
+      className={className}
+      onClick={() => alert(`Hello from your ${appName} app!`)}
+    >
+      {children}
+    </button>
+  );
+};
+
+Button.displayName = "MonoUI.Button";
+
+export default Button;
